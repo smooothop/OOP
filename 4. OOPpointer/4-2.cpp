134df3bb983d7f0093a73d0aa9119359450220a5
smooothop <jh3715109@gmail.com>
2024-04-08 15:25:45 +0900

    4장 커밋

--- 4. OOPpointer/4-2.cpp
+++ 4. OOPpointer/4-2.cpp
@@ -21,4 +21,15 @@ int main(){
     circleArray[0].setRadius(10);
     circleArray[1].setRadius(20);
     circleArray[2].setRadius(30);
+
+    for(int i = 0; i < 3; i++){ //배열의 각 원소 객체의 멤버 접근
+        cout << "Circle " << i << "의 면적은 " << circleArray[i].getArea() << endl;
+    }
+
+    Circle *p;
+    p = circleArray; //circleArray 배열 자체가 포인터므로 &연산자 쓰지 않음.
+    for(int i = 0; i < 3; i++){
+        cout << "Circle " << i << "의 면적은 " << p->getArea() << endl;
+        p++;
+    }
 }
